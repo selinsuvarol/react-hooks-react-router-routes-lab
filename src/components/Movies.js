919bf90a1f4942cd9a7d69c1359d8f0d6b4d83ca
Selin Su Varol <selinsuvarol@gmail.com>
2022-07-08 20:35:32 +0300

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>{
+    movies.map(movie=>{
+      return <div key={movie.title}>
+        <h2>{movie.title}</h2>
+        <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map(genre=>{
+            return <li key={genre}>{genre}</li>
+          }
+          )}
+        </ul>
+      </div>
+    })}</div>;
 }
 
 export default Movies;
